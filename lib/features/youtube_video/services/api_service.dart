0045255df7262cd8d8cf7d8926420729876f06ab
priyanshu-creator <priyanshu.creator.dev@gmail.com>
2023-01-11 20:05:02 +0530

    api key resolve

--- lib/features/youtube_video/services/api_service.dart
+++ lib/features/youtube_video/services/api_service.dart
@@ -1,11 +1,13 @@
-import 'package:answer_it/utils/key/key.dart';
+
 import 'package:get/get.dart';
 import 'package:youtube_api/youtube_api.dart';
 
 class YTAPIService extends GetxController {
-  static String key = KEY;
+  static String? key = '';
 
-  YoutubeAPI youtube = YoutubeAPI(key);
+  static String EnvKey = String.fromEnvironment('KEY');
+
+  YoutubeAPI youtube = YoutubeAPI(key ?? EnvKey);
   RxList videoResult = [].obs;
   Future<void> callAPI(String search) async {
     videoResult.value = await youtube.search(
